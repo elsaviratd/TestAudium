f9e7296b8f4d841661c6e0cac91ad27511dfc90d
elsaviratd <elsavira177@gmail.com>
2024-09-18 08:56:43 +0700

    Initial Commit

---
+++ TestAudium/ContentView.swift
@@ -0,0 +1,60 @@
+//
+//  ContentView.swift
+//  TestAudium
+//
+//  Created by Elsavira T on 17/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    var museumName = ["Select Museum", "Kapal Lawd Museum"]
+    @State private var selectedMuseum = "Select Museum"
+    @State private var selectedSession = false
+    
+    var body: some View {
+        NavigationView {
+            List {
+                Section(header: Text("Session")) {
+                    HStack {
+                        Text("Museum")
+                        Spacer()
+                        Picker("", selection: $selectedMuseum) {
+                            ForEach(museumName, id: \.self) {
+                                Text($0)
+                            }
+                        }
+                        .pickerStyle(.automatic)
+                    }
+                    
+                    HStack {
+                        Toggle(isOn: $selectedSession) {
+                            Text("Session")
+                        }
+                    }
+                }
+                
+                Section(header: Text("Debug Info")) {
+                    HStack {
+                        Text("Nearest Beacon")
+                    }
+                    HStack {
+                        Text("Beacon ID")
+                    }
+                    HStack {
+                        Text("Current Audio")
+                    }
+                    HStack {
+                        Text("Duration")
+                    }
+                }
+            }
+            .navigationTitle("Audium.")
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TestAudium/TestAudiumApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAudiumApp.swift
+//  TestAudium
+//
+//  Created by Elsavira T on 17/09/24.
+//
+
+import SwiftUI
+
+@main
+struct TestAudiumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
